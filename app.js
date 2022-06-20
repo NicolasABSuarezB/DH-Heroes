c506a7188faef801fcb632bb34732c422cb05f75
NicolasABSuarezB <nicosube2014@gmail.com>
2022-06-20 20:32:33 -0300

    Se suben todos los archivos del tp4 Heroes de DH, en ellos se ecuentra la informacion e imagenes de Babbage, Berners-Lee, Clarke, Hamilton, Hooper, Lovelace y Turing

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3030
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')))
+app.get('/turing',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', 'turing.html')))
+app.get('/lovelace',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', 'lovelace.html')))
+app.get('/hooper',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', 'hooper.html')))
+app.get('/hamilton',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', 'hamilton.html')))
+app.get('/clarke',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', 'clarke.html')))
+app.get('/berners-lee',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', 'berners-lee.html')))
+app.get('/babbage',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', 'babbage.html')))
+app.get('/*',(req,res) =>res.sendFile(path.resolve(__dirname, 'views', '404.html')))
+
+app.listen(port, () => console.log(`Se levanto con exito el servidor en http://localhost:${port}`))
